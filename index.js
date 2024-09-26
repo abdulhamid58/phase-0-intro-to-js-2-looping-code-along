2bef7e084876d3ac8cee54f5bb3c620e91526ac3
Abdulhamid <abdulhamidhersi@gmail.com>
2024-09-26 15:46:36 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(names,event) {
+
+    const thankYouMessages = [];
+for(let i = 0; i < names.length; i++){
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+}
+
+    return thankYouMessages
+}
+
+
+
+function countDown(num){
+while(num >= 0){
+    console.log(num);
+
+  num--;
+
+}
+}
+
+console.log(countDown(30))
